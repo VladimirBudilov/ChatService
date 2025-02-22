1269cf299bbda05a93d3a16432db5fe62fbb9835
Vladimir Budilov <budilo.w.ladimir@gmail.com>
2025-02-22 22:55:29 +0400

    feat: add material

--- ChatClient/src/app/app.routes.ts
+++ ChatClient/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
