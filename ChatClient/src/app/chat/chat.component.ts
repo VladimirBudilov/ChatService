3c254a39d55ab4f0de5e67eaa3a923a56eaee9b8
Vladimir Budilov <budilo.w.ladimir@gmail.com>
2025-02-23 00:07:09 +0400

    feat: implement simple chat

--- ChatClient/src/app/chat/chat.component.ts
+++ ChatClient/src/app/chat/chat.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { User } from '../models/User';
+import { Message } from '../models/Message';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {MatFormField} from '@angular/material/form-field';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatList, MatListItem} from '@angular/material/list';
+import {MatButton} from '@angular/material/button';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgForOf,
+    MatFormField,
+    MatSidenavContent,
+    MatListItem,
+    MatList,
+    MatSidenav,
+    MatSidenavContainer,
+    MatButton,
+    MatInput
+  ],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
+  standalone: true,
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  users: User[] = [
+    { id: '1', name: 'Alice' },
+    { id: '2', name: 'Bob' }
+  ];
 
+  messages: Message[] = [
+    { userId: '1', text: 'Hello!' },
+    { userId: '2', text: 'Hi there!' }
+  ];
+
+  messageControl = new FormControl('');
+  currentUserId = localStorage.getItem('userId') || '1';
+
+  ngOnInit() {
+    if (!localStorage.getItem('userId')) {
+      localStorage.setItem('userId', this.currentUserId);
+    }
+  }
+
+  sendMessage() {
+    if (this.messageControl.value) {
+      this.messages.push({
+        userId: this.currentUserId,
+        text: this.messageControl.value
+      });
+      this.messageControl.setValue('');
+    }
+  }
 }
--- ChatClient/src/app/home/home.component.ts
+++ ChatClient/src/app/home/home.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {Router, RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    RouterOutlet
+  ],
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.form = this.fb.group({
+      name: ['']
+    });
+  }
+
+  onSubmit() {
+    const formData = this.form.value;
+    this.http.post('your-backend-url/login', formData).subscribe(() => {
+      this.router.navigate(['/chat']);
+    });
+  }
 }
